8d3e62b216367fc9862e1113a416e8ff2b206dee
rsltrifork <rsl@trifork.com>
2013-04-06 14:40:56 +0200

    Changed to work with Delta Solivia 3.0 EU G4

--- DeltaPVOutput.py
+++ DeltaPVOutput.py
@@ -2,17 +2,17 @@
 #PVoutput.org
 
 import time, subprocess,serial
-from deltaInv import DeltaInverter
+from delta30EUG4TRInv import DeltaInverter
 from time import localtime, strftime
 
 #PVOutput.org API Values - UPDATE THESE TO YOURS!
-SYSTEMID=""
-APIKEY=""
+SYSTEMID="123455"
+APIKEY="a14990baf0290b91eb31a867b6e34531e2a3566b"
 
 if __name__ == '__main__':
 
     #Edit your serial connection as required!!
-    connection = serial.Serial('/dev/ttyUSB0',19200,timeout=0.2);
+    connection = serial.Serial('/dev/ttyUSB0',19200,timeout=0.5);
     localtime = time.localtime(time.time())   
  
     t_date = 'd={0}'.format(strftime('%Y%m%d'))
@@ -20,36 +20,41 @@ if __name__ == '__main__':
 
     inv1 = DeltaInverter(1) #init Inverter 1
     #Get the Daily Energy thus far
-    cmd = inv1.getCmdStringFor('Day Wh')
+    cmd = inv1.getCmdStringFor('Energy Day')
     connection.write(cmd)
     response = connection.read(100)
+
     #if no response the inverter is asleep
     if response:
         value = inv1.getValueFromResponse(response)
         t_energy = 'v1={0}'.format(value)
+        print("t_energy: " + t_energy)
 
-	#instanteous power
+#instanteous power
         cmd = inv1.getCmdStringFor('AC Power')
         connection.write(cmd)
         response = connection.read(100)
         value = inv1.getValueFromResponse(response)
         t_power = 'v2={0}'.format(value)
+        print("t_power: " + t_power)
 
-	#AC Voltage
-        cmd = inv1.getCmdStringFor('AC Volts')
+#AC Voltage
+        cmd = inv1.getCmdStringFor('AC Voltage')
         connection.write(cmd)
         response = connection.read(100)
         value = inv1.getValueFromResponse(response)
         t_volts = 'v6={0}'.format(value)
+        print("t_volts: " + t_volts)
 
-	#Temp - this appears to be onboard somewhere not the heatsink
-        cmd = inv1.getCmdStringFor('DC Temp')
+#Temp - this appears to be onboard somewhere not the heatsink
+        cmd = inv1.getCmdStringFor('Internal Ambient Temperature')
         connection.write(cmd)
         response = connection.read(100)
         value = inv1.getValueFromResponse(response)
         t_temp = 'v5={0}'.format(value)
+        print("t_temp: " + t_temp)
 
-	#Send it all off to PVOutput.org
+#Send it all off to PVOutput.org
         cmd = ['/usr/bin/curl',
             '-d', t_date,
             '-d', t_time,

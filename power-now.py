2812c0b7a22cbde5b702e077cd86b38d188b9f50
rsltrifork <rsl@trifork.com>
2013-04-06 14:40:56 +0200

    Changed to work with Delta Solivia 3.0 EU G4

---
+++ power-now.py
@@ -0,0 +1,57 @@
+#A simple script to read values from a delta inverter and post them to 
+#PVoutput.org
+
+import time, subprocess,serial
+from delta30EUG4TRInv import DeltaInverter
+from time import localtime, strftime
+
+#PVOutput.org API Values - UPDATE THESE TO YOURS!
+SYSTEMID="17458"
+APIKEY="123bc123d42314db1c43b1423d1b2341bc243"
+
+if __name__ == '__main__':
+
+    #Edit your serial connection as required!!
+    connection = serial.Serial('/dev/ttyUSB0',19200,timeout=0.5);
+    localtime = time.localtime(time.time())   
+ 
+    t_date = 'd={0}'.format(strftime('%Y%m%d'))
+    t_time = 't={0}'.format(strftime('%H:%M'))
+
+    inv1 = DeltaInverter(1) #init Inverter 1
+    commandName = 'AC Power'
+    command = inv1.getCmdStringFor(commandName)
+    connection.write(command)
+    response = connection.read(100)
+    p1 = inv1.getValueFromResponse(response)
+    print ("1: " + commandName + ": " + str(p1) + " W")
+
+    inv1 = DeltaInverter(2) #init Inverter 1
+    commandName = 'AC Power'
+    command = inv1.getCmdStringFor(commandName)
+    connection.write(command)
+    response = connection.read(100)
+    p2 = inv1.getValueFromResponse(response)
+    print ("2: " + commandName + ": " + str(p2) + " W")
+
+    print ("Total: " + commandName + ": " + str(int(p1) + int(p2)) + " W\n")
+
+    inv1 = DeltaInverter(1) #init Inverter 1
+    commandName = 'DC Power'
+    command = inv1.getCmdStringFor(commandName)
+    connection.write(command)
+    response = connection.read(100)
+    p1 = inv1.getValueFromResponse(response)
+    print ("1: " + commandName + ": " + str(p1) + " W")
+
+    inv1 = DeltaInverter(2) #init Inverter 1
+    commandName = 'DC Power'
+    command = inv1.getCmdStringFor(commandName)
+    connection.write(command)
+    response = connection.read(100)
+    p2 = inv1.getValueFromResponse(response)
+    print ("2: " + commandName + ": " + str(p2) + " W")
+
+    print ("Total: " + commandName + ": " + str(int(p1) + int(p2)) + " W")
+
+    connection.close()
